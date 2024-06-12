465c349b5c1c4b433f3f1cdf318ba57ffd3f67fd
vao49 <fbrc.le.blanc@gmail.com>
2024-06-12 10:51:01 +0200

    Démo

--- views/continents/index.php
+++ views/continents/index.php
@@ -1,7 +1,6 @@
-<h1>Liste des Continents</h1>
+<h1>Liste des Continents Modif</h1>
 
-<a href="<?= PATH ?>/continents/ajout"><button type='button'
-        class='btn btn-primary bi bi-plus-circle'>&nbsp;Ajouter</button></a><br />
+<a href="<?= PATH ?>/continents/ajout"><button type='button' class='btn btn-primary bi bi-plus-circle'>&nbsp;Ajouter</button></a><br />
 
 <table class="table table-dark table-hover">
     <tr>
@@ -10,18 +9,16 @@
         <th>Actions</th>
     </tr>
 
-    <?php foreach($continents as $continent): ?>
+    <?php foreach ($continents as $continent) : ?>
 
-    <tr>
-        <td><?= $continent['ID_CONTINENT'] ?></td>
-        <td><?= $continent['NOM_CONTINENT'] ?></td>
-        <td>
-            <a href="<?= PATH ?>/continents/modif/<?= $continent['ID_CONTINENT'] ?>"><button
-                    class="btn btn-success bi bi-pencil"></button></a>
-            <a href="<?= PATH ?>/continents/suppr/<?= $continent['ID_CONTINENT'] ?>"><button
-                    class="btn btn-danger bi bi-trash3"></button></a>
-        </td>
-    </tr>
+        <tr>
+            <td><?= $continent['ID_CONTINENT'] ?></td>
+            <td><?= $continent['NOM_CONTINENT'] ?></td>
+            <td>
+                <a href="<?= PATH ?>/continents/modif/<?= $continent['ID_CONTINENT'] ?>"><button class="btn btn-success bi bi-pencil"></button></a>
+                <a href="<?= PATH ?>/continents/suppr/<?= $continent['ID_CONTINENT'] ?>"><button class="btn btn-danger bi bi-trash3"></button></a>
+            </td>
+        </tr>
 
     <?php endforeach ?>
 
